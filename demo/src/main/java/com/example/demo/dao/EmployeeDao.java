629b943e7260b5419bc1495a03c82cdf2da5d57d
LYejin <vgg789@naver.com>
2023-08-15 15:45:13 +0900

    Feat : EmployeeDao 추가

--- demo/src/main/java/com/example/demo/dao/EmployeeDao.java
+++ demo/src/main/java/com/example/demo/dao/EmployeeDao.java
@@ -4,9 +4,26 @@ import com.example.demo.dto.EmployeeDTO;
 import com.example.demo.dto.UserDTO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface EmployeeDao {
 
     public UserDTO userSelect(String userName);
     public void employeesave(EmployeeDTO employeeDTO);
+
+    //전체 사원 리스트 출력
+    public List<EmployeeDTO> employeeSelectAll();
+
+    //사원 상세 테이터 1건 출력
+    public EmployeeDTO employeeDetail();
+
+    //신규 사원 데이터 1건 입력
+    public void employeeInsert();
+
+    //특정 사원 데이터 비 활성화
+    public void employeeRemove();
+
+    //특정 사원 데이터 정보 갱신
+    public void employeeUpdate();
 }
