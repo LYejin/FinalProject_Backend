4ad3a27c1fc396c57b3ba274f7069eb8752a9a6b
NAM DONG WOO <wowdong27@naver.com>
2023-08-16 19:45:30 +0900

    Feat : 사업장 정보 CRUD 추가

--- demo/src/main/java/com/example/demo/controller/WorkplaceController.java
+++ demo/src/main/java/com/example/demo/controller/WorkplaceController.java
@@ -1,20 +1,64 @@
 package com.example.demo.controller;
 
-
-import com.example.demo.dto.EmployeeDTO;
-import com.example.demo.service.EmployeeService;
+import com.example.demo.dto.WorkplaceDTO;
 import com.example.demo.service.WorkplaceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("system/user/personnelManage/")
-@CrossOrigin("http://localhost:3000")
-public class WorkplaceController {
-    @Autowired
-    private WorkplaceService workplaceService;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+@RestController
+@RequestMapping("system/user/WorkplaceManage/")
+public class WorkplaceController {
+
+    private final WorkplaceService workplaceService;
+
+    @Autowired
+    public WorkplaceController(WorkplaceService workplaceService) {
+        this.workplaceService = workplaceService;
+    }
+
+    @GetMapping("getList")
+    public ResponseEntity<List<WorkplaceDTO>> getAllWorkplaces(
+            @RequestParam(name = "DIV_CD", required = false) String DIV_CD,
+            @RequestParam(name = "CO_CD", required = false) String CO_CD,
+            @RequestParam(name = "DIV_YN", required = false) String DIV_YN
+    ) {
+        Map<String,String> map = new HashMap<>();
+        map.put("DIV_YN",DIV_CD);
+        map.put("CO_CD",CO_CD);
+        map.put("DIV_YN",DIV_YN);
+        List<WorkplaceDTO> list = null;
+        list = workplaceService.selectWorkplaceSearch(map);
+        return new ResponseEntity<List<WorkplaceDTO>>(list,HttpStatus.OK);
+    }
+
+    @GetMapping("getInfo/{divCd}")
+    public ResponseEntity<WorkplaceDTO> getWorkplaceInfo(@PathVariable String divCd) {
+        WorkplaceDTO wpInfo = null;
+        wpInfo = workplaceService.selectWorkplaceInfoByDIVCD(divCd);
+        return new ResponseEntity<WorkplaceDTO>(wpInfo, HttpStatus.OK);
+    }
+
+    @PostMapping("insert")
+    public ResponseEntity<Integer> InsertWorkplace(@RequestBody WorkplaceDTO workplaceDTO) {
+        int result = workplaceService.insertWorkplace(workplaceDTO);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @PutMapping("update")
+    public ResponseEntity<Integer> updateWorkplace(@RequestBody WorkplaceDTO workplaceDTO) {
+        int result = workplaceService.updateWorkplace(workplaceDTO);
+            return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @PutMapping("delete/{divCd}")
+    public ResponseEntity<Integer> deleteWorkplace(@PathVariable String divCd) {
+        int result = workplaceService.deleteWorkplace(divCd);
+            return new ResponseEntity<>(result, HttpStatus.OK);
+    }
 }
--- demo/src/main/java/com/example/demo/dao/WorkplaceDao.java
+++ demo/src/main/java/com/example/demo/dao/WorkplaceDao.java
@@ -2,10 +2,27 @@ package com.example.demo.dao;
 
 import com.example.demo.dto.EmployeeDTO;
 import com.example.demo.dto.UserDTO;
+import com.example.demo.dto.WorkplaceDTO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface WorkplaceDao {
 
+    //사업장 목록 가져오기
+    public List<WorkplaceDTO> selectWorkplaceSearch(Map<String,String> map);
 
+    //사업장 상세정보 가져오기
+    public WorkplaceDTO selectWorkplaceInfoByDIVCD(String DIV_CD);
+
+    //사업장 등록(추가)
+    public int insertWorkplace(WorkplaceDTO workplaceDTO);
+
+    //사업장 수정
+    public int updateWorkplace(WorkplaceDTO workplaceDTO);
+
+    //사업장 삭제
+    public int deleteWorkplace(String DIV_CD);
 }
