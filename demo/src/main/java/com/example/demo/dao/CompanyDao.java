539f2328c351d18dbaf1679099948110df80d8e1
songhasuk <shs1995@naver.com>
2023-08-18 17:43:14 +0900

    Feat : 회사 관련 MVC 형태 파일 생성

--- demo/src/main/java/com/example/demo/dao/CompanyDao.java
+++ demo/src/main/java/com/example/demo/dao/CompanyDao.java
@@ -1,11 +1,27 @@
 package com.example.demo.dao;
 
+import com.example.demo.dto.CompanyDTO;
 import com.example.demo.dto.EmployeeDTO;
 import com.example.demo.dto.UserDTO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface CompanyDao {
 
+    //전체 회사 리스트 출력
+    public List<CompanyDTO> companySelectAll();
 
+    //회사 상세 테이터 1건 출력
+    public CompanyDTO companyDetail(String co_CD);
+
+    //신규 회사 데이터 1건 입력
+    public void companyInsert(CompanyDTO companyDTO);
+
+    //특정 회사 데이터 비 활성화
+    public void companyRemove(String CO_CD);
+
+    //특정 회사 데이터 정보 갱신
+    public void companyUpdate(CompanyDTO companyDTO);
 }
--- demo/src/main/java/com/example/demo/dto/CompanyDTO.java
+++ demo/src/main/java/com/example/demo/dto/CompanyDTO.java
@@ -1,24 +1,34 @@
 package com.example.demo.dto;
 
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class CompanyDTO {
-    private String coCd;
-    private String useYn;
-    private String coNm;
-    private String coNmk;
-    private String business;
-    private String jongmok;
-    private String regNb;
-    private String hoFax;
-    private String ceoNm;
-    private String hoZip;
-    private String hoAddr;
-    private String hoAddr1;
-    private String coFg;
-    private String coNb;
-    private String estDt;
-    private String openDt;
-    private String closeDt;
+    private String CO_CD;           //회사코드
+    private String USE_YN;          //사용여부
+    private String CO_NM;           //회사명
+    private String CO_NMK;          //회사약칭
+    private String BUSINESS;        //업태
+    private String JONGMOK;         //종목
+    private String REG_NB;          //사업자번호
+    private String CEO_NM;          //대표자명
+    private String HO_FAX;          //대표자팩스
+    private String CEO_TEL;         //대표자전화번호
+    private String PPL_NB;          //대표자주민번호
+    private String HO_ZIP;          //본점우편번호
+    private String HO_ADDR;         //본점주소
+    private String HO_ADDR1;        //본점번지
+    private String CO_FG;           //회사구분
+    private String CO_NB;           //법인드록번호
+    private String EST_DT;          //설립일
+    private String OPEN_DT;         //개업일
+    private String CLOSE_DT;        //폐업일
+    private String PIC_FILE_ID;     //사진파일명 byte[]
+    private String ACCT_FG;         //회사계정유형
+
+    //private MultipartFile PIC_FILE_ID; //byte처리 전 데이터
+
+
+
 }
