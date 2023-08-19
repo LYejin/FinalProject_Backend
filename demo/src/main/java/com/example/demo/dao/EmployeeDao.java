cb2d27347b73d8e40a5769d00278130d56b8d3dd
LYejin <vgg789@naver.com>
2023-08-19 17:54:50 +0900

    Fix : InsertEmployeeWithRollProcedure 로 수정

--- demo/src/main/java/com/example/demo/dao/EmployeeDao.java
+++ demo/src/main/java/com/example/demo/dao/EmployeeDao.java
@@ -1,6 +1,5 @@
 package com.example.demo.dao;
 
-import com.example.demo.dto.CompanyDTO;
 import com.example.demo.dto.EmployeeDTO;
 import com.example.demo.dto.UserDTO;
 import org.apache.ibatis.annotations.Mapper;
