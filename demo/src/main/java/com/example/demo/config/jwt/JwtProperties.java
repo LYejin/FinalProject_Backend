18223d29d3b3424ef757579a9236918c6fc65c16
LYejin <vgg789@naver.com>
2023-09-19 20:55:39 +0900

    Feat : RefreshToken 기능 구현

--- demo/src/main/java/com/example/demo/config/jwt/JwtProperties.java
+++ demo/src/main/java/com/example/demo/config/jwt/JwtProperties.java
@@ -5,8 +5,8 @@ import java.util.Base64;
 public interface JwtProperties {
     String SECRET = "서버에기록된비밀번호"; // 서버만 알고 있는 비밀값
 
-    int ACCESS_EXPIRATION_TIME = 60 * 60 * 1000;  //60 * 60 * 1000
-    int REFRESH_EXPIRATION_TIME = 604800000; // 10일 (1/1000초)
+    int ACCESS_EXPIRATION_TIME = 60 * 60 * 1000; // 1시간
+    int REFRESH_EXPIRATION_TIME = 14 * 24 * 60 * 60 * 1000; // 2주
     String TOKEN_PREFIX = "Bearer "; //USERNAME과 USERID 정보만 가짐
     String HEADER_STRING = "Authorization"; //
 
