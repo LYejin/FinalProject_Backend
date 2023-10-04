f26e8b74b77d16943d546458a88c052e099a4b98
songhasuk <shs1995@naver.com>
2023-10-04 10:16:38 +0900

    Feat : 로그인 정보 dto추가 및 병합

---
+++ demo/src/main/java/com/example/demo/dto/LoginUserInfoDTO.java
@@ -0,0 +1,12 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginUserInfoDTO {
+    private String KOR_NM;
+    private String PIC_FILE_ID;
+    private String CO_NM;
+    private String DEPT_NM;
+    private String ROLE_NAME;
+}
