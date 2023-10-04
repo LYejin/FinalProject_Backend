db13688ac8a1f8ea31d5d2c7e523370da13ecf2e
songhasuk <shs1995@naver.com>
2023-10-04 10:04:28 +0900

    Feat  : 변경이력 로직 수정

--- demo/src/main/java/com/example/demo/controller/ChangeHistoryController.java
+++ demo/src/main/java/com/example/demo/controller/ChangeHistoryController.java
@@ -1,14 +1,19 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.ChangeHistoryDTO;
+import com.example.demo.dto.ChangeHistoryDetailDTO;
 import com.example.demo.service.ChangeHistoryService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
+
+@Slf4j
 @RestController
 @RequestMapping("system/admin/groupManage/")
 @CrossOrigin("http://localhost:3000")
@@ -26,6 +31,20 @@ public class ChangeHistoryController {
         return new ResponseEntity<List<ChangeHistoryDTO>>(changeHistorySearchDTOS, HttpStatus.OK);
     }
 
+    @PostMapping("ChangeHistoryDetailList")
+    public ResponseEntity<?> ChangeHistoryDetailList(@RequestBody ChangeHistoryDTO changeHistoryDTO){
+        List<ChangeHistoryDetailDTO> changeHistoryDetailDTOS= new ArrayList<>();
+        log.info("서치데이터:"+changeHistoryDTO);
+        changeHistoryDetailDTOS = changeHistoryService.ChangeHistoryDetailList(changeHistoryDTO);
 
+        return new ResponseEntity<List<ChangeHistoryDetailDTO>>(changeHistoryDetailDTOS, HttpStatus.OK);
+    }
+
+    @PostMapping("ChangeHistorySearch")
+    public ResponseEntity<?> ChangeHistorySearch(@RequestBody ChangeHistoryDTO changeHistoryDTO ){
+        List<ChangeHistoryDTO> changeHistorySearchDTOS = null;
+
+        return new ResponseEntity<List<ChangeHistoryDTO>>(changeHistorySearchDTOS, HttpStatus.OK);
+    }
 
 }
